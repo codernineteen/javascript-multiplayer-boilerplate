423f9529452a2aaf77b3c27f31d5597eb5c28906
codernineteen <wjsdPcks34@gmail.com>
2023-03-10 21:19:25 +0900

    scripts: added router without react framework. Google Oauth 2.0 authentication implemented by using passportjs

--- server-udp-express/src/server.ts
+++ server-udp-express/src/server.ts
@@ -1,10 +1,29 @@
-import geckos, { ChannelId, Data, ServerChannel } from "@geckos.io/server";
+//packages
+import geckos, { Data, ServerChannel } from "@geckos.io/server";
 import http from "http";
 import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
+import cors, { CorsOptions } from "cors";
+import dotenv from "dotenv";
+import cookieSession from "cookie-session";
+//classes
 import Player from "./classes/Player.js";
+//modules
+import { connectDB } from "./db/connectMongo.js";
+import { passportSetup } from "./config/passport-setup.js";
+//instances
+import indexRoute from "./routes/index.js";
+import userRoute from "./routes/user.js";
+import passport from "passport";
 
+//Global function execution
+dotenv.config();
+passportSetup(passport);
+
+//Environment variables
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI as string;
+
+//Server instances
 const app = express();
 const server = http.createServer(app);
 const io = geckos({
@@ -12,15 +31,30 @@ const io = geckos({
 });
 const players: Map<string, Player> = new Map();
 
-//config & middleware
-const corsOptions = {
-  origin: "http://localhost:5173",
+//Options
+const whiteList = ["http://localhost:5173"];
+const corsOptions: CorsOptions = {
+  origin: whiteList,
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
-app.use(cors(corsOptions)); //To allow CORS policy
-app.use(bodyParser.json()); //To parser request body
 
-// game-server
+//Middlewares
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["cyberyechan"],
+    maxAge: 24 * 60 * 60 * 100,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cors(corsOptions));
+app.use("/", indexRoute);
+app.use("/auth", userRoute);
+app.use(express.json()); //To parser request body
+
+//Game server
 io.addServer(server);
 io.onConnection((channel: ServerChannel) => {
   console.log(`a user ${channel.id} connected`);
@@ -38,15 +72,25 @@ io.onConnection((channel: ServerChannel) => {
   });
 });
 
+//Application server
 app.post("/leave", (req, res) => {
   const clientId = req.body.key;
   const player = players.get(clientId);
   player?.CleanUp();
 });
 
-// run server
-server.listen(5555, () => {
-  console.log("listening on port : 5555");
-});
+//Run server asynchronously
+const startServer = async () => {
+  try {
+    await connectDB(MONGO_URI);
+    server.listen(PORT || 5555, () => {
+      console.log(`Server listening on port : ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
 // make sure the client uses the same port
 // @geckos.io/client uses the port 9208 by default
